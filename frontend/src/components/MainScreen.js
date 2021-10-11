9bd4d9f7b569e88fa86c927ebe3e7ede12dcf913
Luca Reccia <lucareccia@hotmail.it>
2021-10-11 20:34:14 +0200

    list of owned accounts

--- frontend/src/components/MainScreen.js
+++ frontend/src/components/MainScreen.js
@@ -1,40 +1,76 @@
 import React, { useRef, useState, useEffect } from "react";
-import { AppState, StyleSheet, Text, View, Button, FlatList } from "react-native";
+import { AppState, StyleSheet, Text, SafeAreaView, FlatList, ScrollView, RefreshControl } from "react-native";
+import { ListItem, Avatar, Button } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 export default function Main({navigation}) {
   const [platformAccounts, setPlatformAccounts] = useState([])
+  const [refreshing, setRefreshing] = React.useState(false);
   const api = React.useContext(ApiContext);
 
-  useEffect(() => {
-    const retrievePlatformAccounts = async () => {
-      setPlatformAccounts(await api.getPlatformAccount())
-    }
-    retrievePlatformAccounts()
-  }, [])
-  console.log(platformAccounts)
+  const refreshPlatformAccounts = async () => {
+    setRefreshing(true);
+    setPlatformAccounts(await api.getPlatformAccount())
+    setRefreshing(false);
+  }
+
+  React.useEffect(() => {    
+    return navigation.addListener('focus', () => {      
+      refreshPlatformAccounts()
+    });
+  }, [navigation]);
 
   return (
-    <View style={styles.container}>
-      <Text>Main</Text>
+    <SafeAreaView style={styles.container}>
       <FlatList
+        style={styles.scrollView}
         data={platformAccounts}
-        renderItem={({item:{_id, platform, username}}) => <Text>{platform} {username}</Text>}
+        keyExtractor={(item) => item._id}
+        renderItem={({item:{_id, platform, username}}) => 
+          <ListItem key={_id} bottomDivider>        
+            <Avatar source={require('../../resources/img/instagram-round.png')} />        
+            <ListItem.Content>          
+              <ListItem.Title>{username}</ListItem.Title>          
+              <ListItem.Subtitle>{platform.name}</ListItem.Subtitle>        
+            </ListItem.Content>
+          </ListItem>
+        }
+        numColumns={1}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={refreshPlatformAccounts}
+          />
+        }
       />
-      <Button 
-        title="+"
+      <Button  
+        icon={    
+          <Icon     
+            name="add"
+            size={30}
+            color="white"
+          /> 
+        }
+        iconLeft
+        title=" Add new account"
         onPress={() => {
           navigation.navigate("NewPlatformAccount")
         }}
+        buttonStyle={{
+          margin: 20
+        }}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#aaa',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  scrollView: {
+    flex: 1,
+    width: "100%"
+  }
 });
