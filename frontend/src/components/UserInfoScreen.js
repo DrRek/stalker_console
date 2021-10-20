51b37a128b960e44c3724954ae421c3b8b36d35a
Luca Reccia <lucareccia@hotmail.it>
2021-10-20 12:28:52 +0200

    frontend user page

--- frontend/src/components/UserInfoScreen.js
+++ frontend/src/components/UserInfoScreen.js
@@ -1,77 +1,47 @@
-import React, {useRef, useState, useEffect} from 'react';
-import {AppState, StyleSheet, Text, View, Button} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
 import ApiContext from '../contexts/ApiContext';
 import deviceStorage from '../services/storage.service';
-import Config from "react-native-config";
+import {Avatar} from 'react-native-elements/dist/avatar/Avatar';
+import {Text, Button} from 'react-native-elements';
 
 const axios = require('axios');
 
 export default function UserInfo({navigation}) {
   const {signOut} = React.useContext(ApiContext);
+  const [user, setUser] = React.useState(null);
 
-  const get_auth_headers = async () => {
-    const user = JSON.parse(await deviceStorage.getItem('user'));
-    if (user && user.accessToken) {
-      return {'x-access-token': user.accessToken};
-    } else {
-      return {};
-    }
-  };
-
-  const handleWebSocket = async () => {
-    var ws = new WebSocket(Config.SOCKET, null, { headers: await get_auth_headers()});
-    ws.onopen = () => {
-      console.log('connection open');
+  useEffect(() => {
+    const retrieveUser = async () => {
+      setUser(JSON.parse(await deviceStorage.getItem('user')));
     };
-    ws.onmessage = async ({data}) => {
-      console.log(data);
-      console.log(-2);
-      const {method, url, rawHeaders, data: postData, id} = JSON.parse(data);
-
-      console.log(rawHeaders.length);
-      const headers = {};
-      for (let i = 0; i < rawHeaders.length - 1; i += 2) {
-        headers[rawHeaders[i]] = rawHeaders[i + 1];
-      }
-      headers['accept-encoding'] = 'gzip, deflate';
-      headers['host'] = 'i.instagram.com';
-      console.log(headers['accept-encoding']);
-      console.log(url);
-      console.log(method);
-      console.log(headers);
-      const response = await axios.request({
-        method: method,
-        url: 'https://i.instagram.com' + url,
-        data: postData,
-        headers: headers,
-        decompress: false,
-      });
-      console.log(2);
-      console.log(response.data);
-
-      ws.send(
-        JSON.stringify({
-          id: id,
-          data: response.data,
-          headers: response.headers,
-          status: response.status,
-        }),
-      );
-
-      console.log(response.data);
-      console.log(response.headers);
-      console.log(response.status);
-    };
-    ws.onerror = error => {
-      console.log(error);
-    };
-  };
+    retrieveUser();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>userInfo</Text>
-      <Button style={styles.button} title="web socket" onPress={handleWebSocket} />
-      <Button style={styles.button} title="Logout" onPress={signOut} />
+      <View style={styles.userContainer}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Avatar
+            rounded
+            source={require('../../resources/img/user-avatar.png')}
+            size={150}
+          />
+        </View>
+        <View style={styles.userTextContainer}>
+          <Text h2 style={{color: '#86939e'}}>
+            Hi,
+          </Text>
+          <Text h2 style={{color: '#2089dc', textTransform: 'capitalize'}}>
+            {user && user.username}
+          </Text>
+        </View>
+      </View>
+      <Button
+        title="Logout"
+        icon={{name: 'logout', color: 'white'}}
+        onPress={signOut}
+      />
     </View>
   );
 }
@@ -81,7 +51,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+    color: '#86939e',
+    width: '100%',
+  },
+  userContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    maxHeight: 200,
+    justifyContent: 'space-evenly',
+  },
+  userTextContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'flex-start',
   },
   button: {
     width: 100,
